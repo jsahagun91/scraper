f6844b49ce1c30c424f280731b7d24db9b40f35e
Jose Sahagun <JoseSahagun@Joses-MacBook-Pro-2.local>
2018-07-08 10:49:44 -0700

    mongolab code

--- server.js
+++ server.js
@@ -27,7 +27,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/myScraper");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/myScraper";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+// mongoose.connect("mongodb://localhost/myScraper");
 
 // Routes
 
